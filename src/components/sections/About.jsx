863665c20d0dffd227bd4c35a18b9fa73f179704
Muaz Ahmed <muaz335@gmail.com>
2025-05-04 19:52:30 +0500

    fixed resume link

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -36,7 +36,7 @@ export const About = () => {
                             and create something great together!
                         </p>
                         <div className = "flex justify-center space-x-4">
-                            <a href = "../../assets/ResumeMuazAhmed2025.pdf" className = "bg-indigo-600 text-gray-300 py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] ">
+                            <a href = "https://molarmuaz.github.io/portfolio/assets/ResumeMuazAhmed2025.pdf" className = "bg-indigo-600 text-gray-300 py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] ">
                                 View my Resume
                             </a>
                         </div>
