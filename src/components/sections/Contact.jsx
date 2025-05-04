398988100a486d92f12237ed668411163ac4b5b9
Muaz Ahmed <muaz335@gmail.com>
2025-05-04 20:53:15 +0500

    trying to fix the contact module

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,8 @@
 import { Reveal } from "../Reveal";
 import emailjs from "emailjs-com";
 import React, { useState } from "react";
+import {useRef} from "react";
+const formRef = useRef();   
 
 
 export const Contact = () => {
@@ -15,7 +17,7 @@ export const Contact = () => {
     const submitted = (e) => {
         e.preventDefault();
         emailjs
-            .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_API_KEY)
+            .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, formRef.current, import.meta.env.VITE_API_KEY)
             .then((result)=>{
                 alert("Message sent successfully!")
                 setFormData({name: "", email: "", message: ""})
@@ -28,17 +30,17 @@ export const Contact = () => {
                     <h2 className = "text-4xl font-bold mb-8 bg-gradient-to-r from-indigo-600 to-purple-400 bg-clip-text text-transparent text-center">
                         Get in Touch 
                     </h2>
-                    <form className = "space-y-6" onSubmit={submitted} >
+                    <form className = "space-y-6" onSubmit={submitted} ref={formRef} >
                         <div className = "relative">
-                            <input value={formData.name} onChange={(e)=>setFormData({...formData, name: e.target.value})} type = "text" id = "name" name = "name" required placeholder="Name" className = "w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-non focus:border-indigo-500 focus:bg-indigo-500/5 "/>
+                            <input type = "text" id = "name" name = "name" required placeholder="Name" className = "w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-non focus:border-indigo-500 focus:bg-indigo-500/5 "/>
                         </div>
 
                         <div className = "relative">
-                            <input value={formData.email} onChange={(e)=>setFormData({...formData, email: e.target.value})} type = "email" id = "email" name = "email" required placeholder="Email" className = "w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-non focus:border-indigo-500 focus:bg-indigo-500/5 "/>
+                            <input type="email" id="email" name="email" required placeholder="Email" className = "w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-non focus:border-indigo-500 focus:bg-indigo-500/5 "/>
                         </div>
 
                         <div className = "relative">
-                            <textarea value={formData.message} onChange={(e)=>setFormData({...formData, message: e.target.value})} id = "message" name = "message" required rows = {5} placeholder="Message" className = "w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-non focus:border-indigo-500 focus:bg-indigo-500/5 "/>
+                            <textarea type="message" id = "message" name = "message" required rows = {5} placeholder="Message" className = "w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-non focus:border-indigo-500 focus:bg-indigo-500/5 "/>
                         </div>
 
                         <button type = "submit" className = "w-full bg-gradient-to-r from-purple-400 to-indigo-600 text-white font-medium py-3 px-6 rounded hover:from-purple-500 to-indigo-700 transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Submit</button>
